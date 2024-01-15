d7d623fe464ed51d210f568957ecb40fa72cb7d9
Andrew G. Knackstedt <knackstedt@outlook.com>
2024-01-14 21:22:22 -0500

    🚀feat(app) add Vite builder and export

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+// vite.config.js
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+    plugins: [dts({ rollupTypes: true })],
+    server: {
+        port: 4300,
+        strictPort: true
+    },
+    build: {
+        lib: {
+            // Could also be a dictionary or array of multiple entry points
+            entry: resolve(__dirname, 'src/main.ts'),
+            name: 'MatrixAnimation',
+            // the proper extensions will be added
+            fileName: 'matrix-animation',
+            formats: ['es', 'cjs', 'umd'],
+            
+        },
+        sourcemap: true
+    }
+});
